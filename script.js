650492c61a50fae21eb3eac2b9829abf6c21602b
JordanFranck <jordan.franckbm@gmail.com>
2025-02-19 11:05:13 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+let numMes = prompt("Digite o número do mês do seu aniversário;");
+let mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro",];
+
+alert("Você nasceu no mês de "+ mes [numMes -1]);
